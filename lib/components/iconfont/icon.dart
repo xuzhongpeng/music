815798e965e651d555e4cd89e2a8e05345220150
xuzhongpeng <1452754335@qq.com>
2020-04-22 09:46:31 +0800

    feat: 添加图标

---
+++ lib/components/iconfont/icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:music/components/iconfont/iconfont.dart';
+
+class JUIcon {
+  static Widget singleCycle() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[Icon(IconFont.iconshunxu), Text('1')],
+    );
+  }
+}
--- lib/components/iconfont/iconfont.dart
+++ lib/components/iconfont/iconfont.dart
@@ -3,6 +3,10 @@ import 'package:flutter/widgets.dart';
 class IconFont {
   static const String _family = 'iconfont';
   IconFont._();
+  static const IconData icondanquxunhuan =
+      IconData(0xe612, fontFamily: _family);
+  static const IconData iconshunxu = IconData(0xe610, fontFamily: _family);
+  static const IconData iconsuiji = IconData(0xe61d, fontFamily: _family);
   static const IconData iconzanting = IconData(0xe625, fontFamily: _family);
   static const IconData iconbofang = IconData(0xe60f, fontFamily: _family);
   static const IconData iconshangyishou = IconData(0xe603, fontFamily: _family);
